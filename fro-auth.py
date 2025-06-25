2a39f3fc02da18af66919a2c938b9b37c0da4d9f
admin-aavapti <mohit@aavaptitechnologies.com>
2025-06-25 12:58:31 +0530

    Update fro-auth.py

--- fro-auth.py
+++ fro-auth.py
@@ -7,7 +7,7 @@ import os
 def _add_custom_authoring_mfe(mfes):
     gh_pat = os.environ.get("GH_PAT")
     mfes["authoring"] = {
-        "repository": f"https://admin-aavapti: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@github.com/admin-aavapti/frontend-app-authoring.git",
+        "repository": f"https://admin-aavapti:{gh_pat}@github.com/admin-aavapti/frontend-app-authoring.git",
         "port": "9876",
         "version": "sumac.2",
     }
