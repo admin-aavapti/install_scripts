e8904f96af542ca260340b17b185aab6871f961c
admin-aavapti <mohit@aavaptitechnologies.com>
2025-06-25 12:40:18 +0530

    Update fro-auth.py

--- fro-auth.py
+++ fro-auth.py
@@ -1,11 +1,13 @@
 import pluggy
 from tutor import hooks
 from tutormfe.hooks import MFE_APPS
+import os
 
 @MFE_APPS.add()
 def _add_custom_authoring_mfe(mfes):
+    git_token = os.environ.get("GIT_TOKEN", "")
     mfes["authoring"] = {
-        "repository": "https://github.com/yashaswi29/frontend-app-authoring.git",
+        "repository": f"https://admin-aavapti:{GH_PAT}@github.com/admin-aavapti/frontend-app-authoring.git",
         "port": "9876",
         "version": "sumac.2",
     }
