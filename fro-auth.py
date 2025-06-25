4c5be61ef7c33321024d1369d3f10f39ac8d20ed
admin-aavapti <mohit@aavaptitechnologies.com>
2025-06-25 13:02:37 +0530

    hard coded the token to clone

--- fro-auth.py
+++ fro-auth.py
@@ -5,7 +5,6 @@ import os
 
 @MFE_APPS.add()
 def _add_custom_authoring_mfe(mfes):
-    gh_pat = os.environ.get("GH_PAT")
     mfes["authoring"] = {
         "repository": f"https://admin-aavapti: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@github.com/admin-aavapti/frontend-app-authoring.git",
         "port": "9876",
